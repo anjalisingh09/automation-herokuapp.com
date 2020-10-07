3437363e98792883654080b81ba4d3f60f9823fa
anjalisingh09 <anjaliisingh3@gmail.com>
2020-10-07 15:45:32 +0530

    Add. HeadingPage

---
+++ src/test/java/pages/HeadingPage.java
@@ -0,0 +1,40 @@
+package pages;
+import framework.LoggerClass;
+import framework.Wait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.List;
+public class HeadingPage extends LoggerClass {
+    @FindBy(css = "h1.heading")
+    WebElement heading;
+    @FindBy(css = "a[href='/add_remove_elements/']")
+    WebElement addOrRemove;
+    @FindBy(xpath = "//*[@id=\"content\"]/div/button")
+    WebElement addElement;
+    @FindBy(css = "h3")
+    WebElement addRemoveHeading;
+    @FindBy(css = "button.added-manually")
+    List<WebElement> deleteElement;
+    public HeadingPage() {
+        PageFactory.initElements(driver, this);
+    }
+    public String displayHeading() {
+        Wait.explicitWait(ExpectedConditions.visibilityOf(heading));
+        return heading.getText();
+    }
+    public String addOrRemoveElement(){
+        Wait.explicitWait(ExpectedConditions.elementToBeClickable(addOrRemove));
+        addOrRemove.click();
+        System.out.println("Addo or remove");
+        Wait.explicitWait(ExpectedConditions.elementToBeClickable(addElement));
+        addElement.click();
+        System.out.println("Button");
+        Wait.explicitWait(ExpectedConditions.elementToBeClickable(addElement));
+        addElement.click();
+        Wait.explicitWait(ExpectedConditions.elementToBeClickable(deleteElement.get(0)));
+        deleteElement.get(0).click();
+        return addRemoveHeading.getText();
+    }
+}
